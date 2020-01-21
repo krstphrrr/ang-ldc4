fdb6d8c0c7fbef78a69c5a330aa54757d97309a8
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-01-21 15:33:50 -0700

    need to extend leaflet control classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { LearnComponent } from './learn/learn.component';
+import { DataComponent } from './data/data.component';
+import { ToolsComponent } from './tools/tools.component';
+import { FolderComponent } from './folder/folder.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent,
+    LearnComponent,
+    DataComponent,
+    ToolsComponent,
+    FolderComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
