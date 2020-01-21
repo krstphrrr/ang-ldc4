0055fad61ef0a8a9b79e47116ee27b7f437ce459
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-01-21 16:54:13 -0700

    controlextend work

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,7 +9,7 @@ import * as L from 'leaflet'
     './map.component.css']
 })
 export class MapComponent implements OnInit, AfterViewInit {
-  private map;
+  private mymap;
 
   constructor() { }
 
@@ -22,15 +22,41 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+    this.mymap = L.map('map', {
+      center: [ 32.344147, -106.758442 ],
+      zoom: 10
     });
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+      maxZoom: 13,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     })
-    tiles.addTo(this.map);
+    tiles.addTo(this.mymap);
+
+    let testControl = L.Control.extend({
+      options: {
+        position: "bottomleft"
+      },
+
+      onAdd: function (map) {
+        return L.DomUtil.create('div', 'custom-item')
+      },
+      // setContent: function(content){
+      //   this.getContainer().innerHTML = content
+      // }
+    })
+
+    
+
+    L.testControl = function(options){
+      return new L.TestControl(options)
+    }
+
+    L.testControl({position:'bottomleft'}).addTo(this.mymap)
+
+
+    
+    
+    
   }
 
   
