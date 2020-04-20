c621471daaef2afc5add5ab15096b4d056d10498
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-04-20 10:56:46 -0600

    working client side socket

--- src/app/learn/socketTest.service.ts
+++ src/app/learn/socketTest.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client'
 import { Observable } from 'rxjs'
 
+
 @Injectable({providedIn:'root'})
 
 export class socketDataService {
   socket: any
   readonly url:string = 'https://new.landscapedatacommons.org/socket'
-
+  // readonly url:string = 'http://localhost:5000' DEV
   constructor(){
-    this.socket = io.connect(this.url)
+    this.socket = io(this.url,{path:'/ws2'}).connect()
+    // this.socket = io.connect(this.url) DEV
   }
 
 // this.socket.listen(eventname) to
