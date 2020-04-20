b2d2ffbf461f27aba016fa5b53b69df29aa153ff
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-04-20 12:01:48 -0600

    socket url rollback (conflict w apache rev proxy)

--- src/app/learn/socketTest.service.ts
+++ src/app/learn/socketTest.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs'
 
 export class socketDataService {
   socket: any
-  readonly url:string = 'https://new.landscapedatacommons.org/'
+  readonly url:string = 'https://new.landscapedatacommons.org/socket'
   // readonly url:string = 'http://localhost:5000' DEV
   constructor(){
     this.socket = io(this.url,{path:'/ws2'}).connect()
