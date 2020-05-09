119d3da5e1fe575340b3958a68f2597c8e05bd31
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-05-08 19:21:53 -0600

    checkbox logic

--- src/app/map/summary-table/summary-table.component.ts
+++ src/app/map/summary-table/summary-table.component.ts
@@ -1,9 +1,11 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections'
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { SummaryTableDataSource, SummaryTableItem } from './summary-table-datasource';
 import { CustomControlService } from '../../services/custom-control.service'
+
 
 @Component({
   selector: 'app-summary-table',
@@ -15,9 +17,10 @@ export class SummaryTableComponent implements AfterViewInit, OnInit {
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<SummaryTableItem>;
   dataSource: SummaryTableDataSource;
+  selection = new SelectionModel<SummaryTableItem>(true, []);
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['project', 'length'];
+  displayedColumns = ['select','project', 'length'];
   constructor(private dataBus:CustomControlService){}
 
   ngOnInit() {
@@ -29,4 +32,18 @@ export class SummaryTableComponent implements AfterViewInit, OnInit {
     // this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+isAllSelected() {
+  const numSelected = this.selection.selected.length;
+  const numRows = this.dataSource.data.length;
+  return numSelected == numRows;
+}
+
+/** Selects all rows if they are not all selected; otherwise clear selection. */
+masterToggle() {
+  this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+}
 }
