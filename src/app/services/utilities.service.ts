54262903173be532a41327521adb6a54c18152e4
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-03-15 15:51:19 -0600

    segragating xmlbuilder into separate functions

--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -76,4 +76,86 @@ export class UtilitiesService {
       console.log(`Table '${table}' not implemented!`)
     }    
   }
+  setupXML(table,dat){
+    let jsonfile = dat.default[table]
+    console.log(jsonfile)
+    let root = create({version:'1.0', encoding:'UTF-8'})
+    .ele('metadata')
+      .ele('idinfo')
+        .ele('citation')
+          .ele('citeinfo')
+            .ele('origin').txt(jsonfile.idinfo.citation.citeinfo.origin).up()
+            .ele('pubdate').txt(jsonfile.idinfo.citation.citeinfo.pubupdate).up()
+            .ele('title').txt(jsonfile.idinfo.citation.citeinfo.title).up()
+            .ele('geoform').txt(jsonfile.idinfo.citation.citeinfo.geoform).up()
+            .ele('onlink').txt(jsonfile.idinfo.citation.citeinfo.onlink).up()
+            .up()
+          .up()
+        .up()
+        .ele('descript')
+          .ele('abstract').txt(jsonfile.idinfo.descript.abstract).up()
+          .ele('purpose').txt(jsonfile.idinfo.descript.purpose).up()
+          .ele('supplinf').txt(jsonfile.idinfo.descript.supplinf).up()
+          .up()
+          .ele('timeperd')
+            .ele('timeinfo')
+              .ele('sngdate')
+                .ele('caldate').txt(jsonfile.idinfo.timeperd.timeinfo.sngdate.caldate).up()
+                .up()
+              .up()
+              .ele('current').txt(jsonfile.idinfo.current).up()
+          .up()
+          .ele('status')
+            .ele('progress').txt(jsonfile.idinfo.status.progress).up()
+            .ele('update').txt(jsonfile.idinfo.status.update).up()
+          .up()
+    return root
+  }
+
+  variableXML(xmlRoot, table, dat){
+    // xml section of variable structure
+    let full = dat.default[table]
+    if(Object.keys(full).includes('keywords')){
+      // inside the keywords section of the json
+      let themeCount = 0
+      for(let i of Object.keys(full.keywords)){
+        // creating however many 'theme' 
+        xmlRoot.ele("theme")
+          if(Object.keys(full.idinfo.keywords[i]).includes('themekt')){
+            for(let j of Object.keys(full.keywords[i])){
+              if(j.includes('themekt')){
+                console.log(full.keywords[i])
+                xmlRoot.ele(j).txt(full.keywords[i][j]).up()
+              } else if(/[0-9]/g.test(j)){
+                xmlRoot.ele(j.replace(/[0-9]/g,'')).txt(full.keywords[i][j]).up()
+              }
+            }
+          }
+      }
+    //
+    xmlRoot.ele("accconst").txt(full["accconst"]).up()
+    xmlRoot.ele("useconst").txt(full["useconst"]).up()
+    xmlRoot.ele("ptcontact")
+            .ele("cntinfo")
+              .ele("cntperp")
+                .ele("cntper").txt(full.ptcontact.cntinfo.cntperp.cntper).up()
+                .ele("cntorg").txt(full.ptcontact.cntinfo.cntperp.cntorg).up().up()
+              .ele("cntpos").txt(full.ptcontact.cntinfo.cntpos).up()
+              .ele("cntaddr")
+                .ele("addrtype").txt(full.ptcontact.cntinfo.cntaddr.addrtype).up()
+                .ele("address").txt(full.ptcontact.cntinfo.cntaddr.address).up()
+                .ele("city").txt(full.ptcontact.cntinfo.cntaddr.city).up()
+                .ele("state").txt(full.ptcontact.cntinfo.cntaddr.state).up()
+                .ele("postal").txt(full.ptcontact.cntinfo.cntaddr.postal).up()
+                .ele("country").txt(full.ptcontact.cntinfo.cntaddr.country).up().up()
+
+    } else {
+      console.log('no')
+    }
+    // .up()
+    return xmlRoot
+  }
+  finishXML(incXML){
+    return incXML.end({pretty:true})
+  }
 }
