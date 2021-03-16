07068f1dc9fa51f70748fc3252e159da8d5ad270
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-03-16 16:02:42 -0600

    added assembler to utilities

--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -1,3 +1,4 @@
+import { variable } from '@angular/compiler/src/output/output_ast';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import {create} from 'xmlbuilder2'
@@ -20,65 +21,24 @@ export class UtilitiesService {
     this.option.next(str)
   }
 
-  xmlCreate(table,dat){
-    if(Object.keys(dat.default).includes(table)){
-      let jsonfile = dat.default[table]
-      let docu = create({version:'1.0', encoding:'UTF-8'})
-      .ele('metadata')
-        .ele('idinfo')
-          .ele('citation')
-            .ele('citeinfo')
-              .ele('origin').txt(jsonfile.citeinfo.origin).up()
-              .ele('pubdate').txt(jsonfile.citeinfo.pubupdate).up()
-              .ele('title').txt(jsonfile.citeinfo.title).up()
-              .ele('geoform').txt(jsonfile.citeinfo.geoform).up()
-              .ele('onlink').txt(jsonfile.citeinfo.onlink).up()
-              .up()
-            .up()
-          .up()
-          .ele('descript')
-            .ele('abstract').txt(jsonfile.descript.abstract).up()
-            .ele('purpose').txt(jsonfile.descript.purpose).up()
-            .ele('supplinf').txt(jsonfile.descript.supplinf).up()
-            .up()
-            .ele('timeperd')
-              .ele('timeinfo')
-                .ele('sngdate')
-                  .ele('caldate').txt(jsonfile.timeperd.timeinfo.sngdate.caldate).up()
-                  .up()
-                .up()
-                .ele('current').txt(jsonfile.timeperd.current).up()
-              .up()
-            .ele('status')
-              .ele('progress').txt(jsonfile.status.progress).up()
-              .ele('update').txt(jsonfile.status.update).up().up()
-            .ele("spdom")
-              .ele("bounding")
-                  .ele("westbc").up()
-                  .ele("eastbc").up()
-                  .ele("northbc").up()
-                  .ele("southbc").up()
-                .up()
-            .ele("keywords")
-              .ele("theme")
-                .ele("themekt").txt(jsonfile.keywords.theme1.themekt).up()
-                .ele("themekey").txt(jsonfile.keywords.theme1.themekey1).up()
-                .ele("themekey").txt(jsonfile.keywords.theme1.themekey2).up()
-                .ele("themekey").txt(jsonfile.keywords.theme1.themekey3).up()
-                .up()
-              .ele("theme")
-                .ele("themekt").txt(jsonfile.keywords.theme2.themekt).up()
-                  .ele("themekey").txt(jsonfile.keywords.theme2.themekey1).up()
-            // end 
-            .up()
-            return docu.end({ prettyPrint: true })
-    } else{
-      console.log(`Table '${table}' not implemented!`)
-    }    
+  xmlAssembler(table, json){
+    console.log(table,json)
+    let setup = this.setupXML(table,json)
+    console.log(setup)
+    let variablePart = this.variableXML(setup,table,json)
+    console.log(variablePart)
+    let dataqual = this.dataqualXML(variablePart, table, json)
+    console.log(dataqual)
+    let spdoinfo = this.spdoinfoXML(dataqual, table, json)
+    console.log(spdoinfo)
+    let spref = this.sprefXML(spdoinfo,table, json)
+    console.log(spref)
+    let finalxml = this.finishXML(spref)
+    console.log(finalxml)
+    return finalxml
   }
   setupXML(table,dat){
     let jsonfile = dat.default[table]
-    console.log(jsonfile)
     let root = create({version:'1.0', encoding:'UTF-8'})
     .ele('metadata')
       .ele('idinfo')
@@ -115,39 +75,41 @@ export class UtilitiesService {
   variableXML(xmlRoot, table, dat){
     // xml section of variable structure
     let full = dat.default[table]
-    if(Object.keys(full).includes('keywords')){
+    if(Object.keys(full.idinfo).includes('keywords')){
       // inside the keywords section of the json
       let themeCount = 0
-      for(let i of Object.keys(full.keywords)){
+      for(let i of Object.keys(full.idinfo.keywords)){
         // creating however many 'theme' 
         xmlRoot.ele("theme")
           if(Object.keys(full.idinfo.keywords[i]).includes('themekt')){
-            for(let j of Object.keys(full.keywords[i])){
+            for(let j of Object.keys(full.idinfo.keywords[i])){
               if(j.includes('themekt')){
-                console.log(full.keywords[i])
-                xmlRoot.ele(j).txt(full.keywords[i][j]).up()
+                console.log(full.idinfo.keywords[i])
+                xmlRoot.ele(j).txt(full.idinfo.keywords[i][j]).up()
               } else if(/[0-9]/g.test(j)){
-                xmlRoot.ele(j.replace(/[0-9]/g,'')).txt(full.keywords[i][j]).up()
+                xmlRoot.ele(j.replace(/[0-9]/g,'')).txt(full.idinfo.keywords[i][j]).up()
               }
             }
           }
       }
     //
-    xmlRoot.ele("accconst").txt(full["accconst"]).up()
-    xmlRoot.ele("useconst").txt(full["useconst"]).up()
+    xmlRoot.ele("accconst").txt(full.idinfo["accconst"]).up()
+    xmlRoot.ele("useconst").txt(full.idinfo["useconst"]).up()
     xmlRoot.ele("ptcontact")
             .ele("cntinfo")
               .ele("cntperp")
-                .ele("cntper").txt(full.ptcontact.cntinfo.cntperp.cntper).up()
-                .ele("cntorg").txt(full.ptcontact.cntinfo.cntperp.cntorg).up().up()
-              .ele("cntpos").txt(full.ptcontact.cntinfo.cntpos).up()
+                .ele("cntper").txt(full.idinfo.ptcontact.cntinfo.cntperp.cntper).up()
+                .ele("cntorg").txt(full.idinfo.ptcontact.cntinfo.cntperp.cntorg).up().up()
+              .ele("cntpos").txt(full.idinfo.ptcontact.cntinfo.cntpos).up()
               .ele("cntaddr")
-                .ele("addrtype").txt(full.ptcontact.cntinfo.cntaddr.addrtype).up()
-                .ele("address").txt(full.ptcontact.cntinfo.cntaddr.address).up()
-                .ele("city").txt(full.ptcontact.cntinfo.cntaddr.city).up()
-                .ele("state").txt(full.ptcontact.cntinfo.cntaddr.state).up()
-                .ele("postal").txt(full.ptcontact.cntinfo.cntaddr.postal).up()
-                .ele("country").txt(full.ptcontact.cntinfo.cntaddr.country).up().up()
+                .ele("addrtype").txt(full.idinfo.ptcontact.cntinfo.cntaddr.addrtype).up()
+                .ele("address").txt(full.idinfo.ptcontact.cntinfo.cntaddr.address).up()
+                .ele("city").txt(full.idinfo.ptcontact.cntinfo.cntaddr.city).up()
+                .ele("state").txt(full.idinfo.ptcontact.cntinfo.cntaddr.state).up()
+                .ele("postal").txt(full.idinfo.ptcontact.cntinfo.cntaddr.postal).up()
+                .ele("country").txt(full.idinfo.ptcontact.cntinfo.cntaddr.country).up().up()
+    xmlRoot.ele("datacred").txt(full.idinfo.datacred)
+    xmlRoot.ele("native").txt(full.idinfo.native).up().up()
 
     } else {
       console.log('no')
@@ -189,6 +151,7 @@ export class UtilitiesService {
   sprefXML(xmlRoot,table,dat){
     let jsonfile = dat.default[table]
     let root = xmlRoot.ele("spref")
+
     if(Object.keys(jsonfile).includes('spref')){
       root.ele("horizsys")
         .ele("geograph")
@@ -201,7 +164,10 @@ export class UtilitiesService {
           .ele("ellips").txt(jsonfile.spref.horizsys.geodetic.ellips).up()
           .ele("semiaxis").txt(jsonfile.spref.horizsys.geodetic.semiaxis).up()
           .ele("denflat").txt(jsonfile.spref.horizsys.geodetic.denflat).up()
+    } else { 
+      console.log("NO HAY SPREF")
     }
+    return root
   }
   finishXML(incXML){
     return incXML.end({pretty:true})
