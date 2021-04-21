df6c5e7f142794cd1aa59db882612c780dbe5321
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-04-21 12:02:54 -0600

    fixed scroll wheel guard directive

--- src/app/directives/scrollfocus.directive.ts
+++ src/app/directives/scrollfocus.directive.ts
@@ -12,28 +12,23 @@ export class ScrollfocusDirective {
     private util:UtilitiesService
   ) {
     
-    const mouseMove = (evt) => {
+    const mouseEnter = () => {
       // function to send scroll-disabling-signal if mouse 
       // falls within the table div region
-      if (this.inScrollRegion(evt)) {
-        this.util.clickGuardSignal(true)
-        
-      } else {
-        this.util.clickGuardSignal(false)
-        
-      }
+        this.util.scrollGuardSignal(true)
     }
+    const mouseLeave = () =>{
+    
+      this.util.scrollGuardSignal(false)
+    }
+
+
+
+
+
     // event listener to the table div element
-    el.nativeElement.addEventListener('mousemove', mouseMove, true);
+    el.nativeElement.addEventListener('mouseenter', mouseEnter, true)
+    el.nativeElement.addEventListener('mouseleave', mouseLeave, true)
    }
 
-   inScrollRegion(evt) {
-    // tracks where the mouse scroll wheel should not function as a map zoom
-    let boxPos = this.el.nativeElement.getBoundingClientRect().x
-    let mousePos = evt.clientX
-    let boxWid = this.el.nativeElement.clientWidth
-    // returns true: mouse position - floating div position is less than the box width 
-    // in other words: is the mouse inside the div that holds the table
-    return mousePos-boxPos < boxWid;
-   }
 }
